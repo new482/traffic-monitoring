57279c8b02f52770b5ec93cde2aab12bb74bcbd4
new482 <new482@gmail.com>
2016-05-27 10:45:23 +0700

    dateTo=dateFrom

--- tfMonitoring/app/controllers/transactions_controller.rb
+++ tfMonitoring/app/controllers/transactions_controller.rb
@@ -59,6 +59,10 @@ class TransactionsController < ApplicationController
     dateFrom = temp1[2].to_s+'-'+temp1[0].to_s+'-'+temp1[1].to_s
     dateTo = temp2[2].to_s+'-'+temp2[0].to_s+'-'+temp2[1].to_s
 
+    if dateTo.nil?
+      dateTo = dateFrom
+    end
+
     # Return all the route in the request range from Hbase
     getRange = $hbaseClient.get("hbase_hive", ["*"], "SingleColumnValueFilter('cf', 'time', "'>='", 'binary:#{dateFrom}') AND "+
               "SingleColumnValueFilter('cf', 'time', "'<='", 'binary:#{dateTo}')", {})
